f787993f538fec7a612f25e41043fa16c9cfb7b8
Alden Bradford <bradfoa@purdue.edu>
2022-02-15 12:17:25 -0500

    baseline model now available

---
+++ baseline_model/make_features/__init__.py
@@ -0,0 +1,25 @@
+"""
+A module to create features based on the Makusafe data set.
+Basic usage would be:
+
+from make_features import make_features
+f = make_features()
+f.to_csv('features.csv')
+
+The module is split into three submodules:
+load_data
+    Acquire the data set, with correct formatting. This may be helpful
+    for other parts of the project as well.
+transformations
+    Holds functions which do not themselves create features, but rather
+    present useful indirect representations of the original data.
+features
+    Define the features and make them available as a data frame.
+
+To contribute, use git to clone the repository into your own directory
+and then use git push to share your commits.
+
+Alden Bradford, January 30 2022
+"""
+from .load_data import load_data
+from .features import make_features
